25acd029f7dcadb24ee6a814920b5c1d8b80fc4e
CRZbulabula <crzbulabula@gmail.com>
2021-06-11 19:07:11 +0800

    update bench

--- src/bench.sh
+++ src/bench.sh
@@ -11,6 +11,15 @@ else
     sudo ./fs ./disk
 fi
 
+# sudo rm -f ./disk/*
+# time dd if=/dev/zero of=./disk/data bs=4k count=2560
+# dd if=./disk/data bs=4k count=2560 of=/dev/null
+
 sudo rm -f ./disk/*
-time dd if=/dev/zero of=./disk/data bs=4k count=2560
-dd if=./disk/data bs=4k count=2560 of=/dev/null
+sudo fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=./disk/data --bs=4k --iodepth=64 --size=10M --readwrite=randrw --rwmixread=75
+sudo rm -f ./disk/*
+sudo fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=./disk/data --bs=4k --iodepth=64 --size=10M --numjobs=2 --readwrite=randrw --rwmixread=75
+sudo rm -f ./disk/*
+sudo fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=./disk/data --bs=4k --iodepth=64 --size=10M --numjobs=4 --readwrite=randrw --rwmixread=75
+sudo rm -f ./disk/*
+sudo fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=test --filename=./disk/data --bs=4k --iodepth=64 --size=10M --numjobs=8 --readwrite=randrw --rwmixread=75
